0f0f5bd1fb47e9e002ca19bd2ec5a9f095361f07
Yucheng Chuang <ducksteven@gmail.com>
2020-03-29 12:30:35 +0900

    Add audiogram.sh

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # amcyfm-utils
+
 Utilities for amcy.fm Podcast Production
+
+## audiogram.sh
+
+Composite video with background image, audio, waveform animation, and subtitles.
+
+### Setup
+
+macOS
+
+```sh
+brew install ffmpeg envsubst
+# Be sure to add /usr/local/opt/gettext/bin to $PATH
+```
+
+### Usage
+
+```sh
+./audiogram.sh -s subtitles.vtt -i image.png -a audio.mp3 -o video.mp4
+```
+
+Any format that is accepted by FFmpeg is accepted, for example:
+
+- `-s` subtitles: `vtt`, `ass`, `srt`
+- `-i` image: `png`, `jpg`
+- `-a` audio: `mp3`, `wav`
+- `-o` output video: `mp4`, `avi`
+
+
---
+++ audiogram.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+set -e
+
+if ! command -v ffmpeg envsubst > /dev/null 2>/dev/null ; then
+  echo "Please install ffmpeg and envsubst" 1>&2
+  echo "ffmpeg: brew install ffmpeg" 1>&2
+  echo "envsubst: brew install gnutext # be sure to add /usr/local/opt/gettext/bin to your \$PATH" 1>&2
+  exit 1
+fi
+
+TEMPLATE=$(cat <<- EOF
+[1:a]
+showwaves=
+  s=840x840:
+  mode=cline:
+  rate=25:
+  colors=black
+[waveform];
+
+[0:v][waveform]
+overlay=
+  x=80:
+  y=80:
+  shortest=1,
+subtitles=
+  \${SUBTITLES}:
+  force_style='PlayResX=1000,PlayResY=1000,FontName=jf-openhuninn-1.0,Fontsize=70,Outline=0,Alignment=4,MarginL=0080,MarginV=0700,PrimaryColour=&H000000&'
+[final]
+EOF
+)
+
+function print_usage {
+  echo "Usage: $(basename $0) -s subtitle.vtt -i image.png -a audio.mp3 -o output.mp4" 1>&2
+}
+
+while getopts 'i:a:o:s:' opt; do
+  case $opt in
+    s) SUBTITLES=$OPTARG ;;
+    a) AUDIO=$OPTARG ;;
+    i) IMAGE=$OPTARG ;;
+    o) OUTPUT=$OPTARG ;;
+    *)
+      print_usage
+      exit 1
+    ;;
+  esac
+done
+
+shift $((OPTIND -1))
+
+if [ -z "$IMAGE" ] || [ -z "$AUDIO" ] || [ -z "$OUTPUT" ] || [ -z "$SUBTITLES" ]; then
+  print_usage
+  exit 1
+fi
+
+tempfile=$(mktemp script.XXXXXX)
+export SUBTITLES
+echo "$TEMPLATE" |  envsubst \$SUBTITLES > "$tempfile"
+
+ffmpeg -loop 1 -i "$IMAGE" -i "$AUDIO" -filter_complex_script "$tempfile" -map "[final]" -map 1:a "$OUTPUT"
+
+unlink "$tempfile"
